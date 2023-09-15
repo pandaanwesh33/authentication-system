dd5b354e2eb749d9af9cfffa54149613d95396b1
pandaanwesh33 <pandaanwesh33@gmail.com>
2023-09-15 13:18:03 +0530

    user registration api added in main

---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+from database import db
+from models import User  # Import your User model
+from passlib.hash import bcrypt  # Import the password hashing library
+
+router = APIRouter()
+
+# pydantic model for validation while creating user
+class UserCreate():
+    username: str
+    email: str
+    password: str
+
+
+@router.post("/register", response_model=User)
+async def user_registration(user: UserCreate):
+    # check if user already exist
+    existing_user = User.select().where(
+        (user.username == User.username) | (user.email == User.email)
+    ).first()
+
+    if(existing_user):
+        raise HTTPException(
+            status_code = 400, 
+            description = "User already exists" 
+        )
+    
+    # hash the password before storing to db
+    hashed_password = bcrypt.hash(user.password)
+
+    # create a new user => create saves new_user to db
+    new_user = User.create(
+        username = user.username,
+        email = user.email,
+        password = hashed_password
+    )
+
+    return new_user
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 [Playhouse](https://docs.peewee-orm.com/en/latest/peewee/playhouse.html#postgres-ext).
+[passlib](https://passlib.readthedocs.io/en/stable/)
+
+### FastAPI
+- [response model](https://fastapi.tiangolo.com/tutorial/response-model/)
